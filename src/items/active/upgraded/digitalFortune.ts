65875ca762c40d6b83326eca34c5619cd7d6e015
somdudewillson <somdudewilson@gmail.com>
2021-05-26 18:13:34 -0700

    Altered "positive" sound effect

--- src/items/active/upgraded/digitalFortune.ts
+++ src/items/active/upgraded/digitalFortune.ts
@@ -45,7 +45,7 @@ export function use(
         true,
       );
     }
-    playSound(SoundEffect.SOUND_THUMBSUP);
+    playSound(SoundEffect.SOUND_SLOTSPAWN);
   } else {
     playSound(SoundEffect.SOUND_THUMBS_DOWN);
   }

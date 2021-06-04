8ac31ce6c2ce8e71a1da6debb31b5fb45d023206
somdudewillson <somdudewilson@gmail.com>
2021-06-04 16:33:39 -0700

    Refactored Tear Reservoir to use throw effect

--- src/items/active/upgraded/t/tearReservoir.ts
+++ src/items/active/upgraded/t/tearReservoir.ts
@@ -1,3 +1,4 @@
+import { queueThrowable } from "../../../../callbacks/handler_ThrownEffect";
 import {
   CollectibleTypeLabUpgrade,
   SHOT_SPEED_MULT,
@@ -17,24 +18,12 @@ export function use(
   _rand: RNG,
   player: EntityPlayer,
   _UseFlags: int,
-  _ActiveSlot: int,
+  ActiveSlot: int,
   _CustomVarData: int,
 ): boolean | { Discharge: boolean; Remove: boolean; ShowAnim: boolean } {
-  const aimDirection = player.GetLastDirection().Rotated(-(FIRE_CONE / 2));
-  const hasCarBattery = player.HasCollectible(
-    CollectibleType.COLLECTIBLE_CAR_BATTERY,
-  );
+  queueThrowable(player, ownType(), ActiveSlot, doFire);
 
-  for (let i = 0; i < (hasCarBattery ? TEAR_COUNT * 2 : TEAR_COUNT); i++) {
-    const fireDir = aimDirection.Rotated(
-      i * (hasCarBattery ? TEAR_ANGLE_INTERVAL / 2 : TEAR_ANGLE_INTERVAL),
-    );
-    fireDir.Resize(player.ShotSpeed * SHOT_SPEED_MULT);
-
-    player.FireTear(player.Position, fireDir, true, true, false, player);
-  }
-
-  return true;
+  return { Discharge: true, Remove: false, ShowAnim: false };
 }
 
 export function postTear(tear: EntityTear): void {
@@ -54,5 +43,26 @@ export function postTear(tear: EntityTear): void {
       player.SetActiveCharge(Math.min(player.GetActiveCharge(s) + 1, 6), s);
       playSound(SoundEffect.SOUND_BEEP, 0.25);
     }
+  }
+}
+
+export function doFire(
+  player: EntityPlayer,
+  directionVector: Vector,
+  _direction: Direction,
+  _data: number,
+): void {
+  const aimDirection = directionVector.Rotated(-(FIRE_CONE / 2));
+  const hasCarBattery = player.HasCollectible(
+    CollectibleType.COLLECTIBLE_CAR_BATTERY,
+  );
+
+  for (let i = 0; i < (hasCarBattery ? TEAR_COUNT * 2 : TEAR_COUNT); i++) {
+    const fireDir = aimDirection.Rotated(
+      i * (hasCarBattery ? TEAR_ANGLE_INTERVAL / 2 : TEAR_ANGLE_INTERVAL),
+    );
+    fireDir.Resize(player.ShotSpeed * SHOT_SPEED_MULT);
+
+    player.FireTear(player.Position, fireDir, true, true, false, player);
   }
 }

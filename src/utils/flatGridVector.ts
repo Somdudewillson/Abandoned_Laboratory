9d50ae1d9c1a1c0ef1b3abdac78a507c7ade49b4
somdudewillson <somdudewilson@gmail.com>
2021-07-02 09:17:35 -0700

    Fixed shift behavior

--- src/utils/flatGridVector.ts
+++ src/utils/flatGridVector.ts
@@ -1,17 +1,23 @@
+const MIN_VALUE = -2;
+const MAX_VALUE = 29;
+const SHIFT = MAX_VALUE - MIN_VALUE + 1;
+
 export type FlatGridVector = number;
 export function flattenVector(inVec: Vector): FlatGridVector {
-  return Math.round(inVec.X + 2) + Math.round(inVec.Y + 2) * 32;
+  return (
+    Math.round(inVec.X - MIN_VALUE) + Math.round(inVec.Y - MIN_VALUE) * SHIFT
+  );
 }
 export function expandVector(inVec: FlatGridVector): Vector {
-  const Y = math.floor(inVec / 32);
-  const X = math.floor(inVec - Y * 32);
+  const Y = math.floor(inVec / SHIFT);
+  const X = math.floor(inVec - Y * SHIFT);
 
-  return Vector(X - 2, Y - 2);
+  return Vector(X + MIN_VALUE, Y + MIN_VALUE);
 }
 export function shiftFlat(
   inVec: FlatGridVector,
   deltaX: int,
   deltaY: int,
 ): FlatGridVector {
-  return inVec + Math.round(deltaX) + Math.round(deltaY * 30);
+  return inVec + Math.round(deltaX) + Math.round(deltaY * SHIFT);
 }

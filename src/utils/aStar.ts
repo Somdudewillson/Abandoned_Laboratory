155726761484897b82e52228432a941ea0bfbe65
somdudewillson <somdudewilson@gmail.com>
2021-07-01 23:48:51 -0700

    Added convienience function to shift flat vectors.

--- src/utils/aStar.ts
+++ src/utils/aStar.ts
@@ -9,6 +9,13 @@ export function expandVector(inVec: FlatGridVector): Vector {
   const Y = math.floor(inVec - X * 28);
   return Vector(X - 1, Y - 1);
 }
+export function shiftFlat(
+  inVec: FlatGridVector,
+  deltaX: int,
+  deltaY: int,
+): FlatGridVector {
+  return inVec + Math.round(deltaX) + Math.round(deltaY * 28);
+}
 
 function reconstructPath(
   cameFrom: Map<FlatGridVector, FlatGridVector>,
